dcc87e0d4b021a0bc7320383853e990a4e7e957e
sameerdewan <sameer.dewan@itential.com>
2021-03-10 10:02:56 -0500

    updates

--- api/server.js
+++ api/server.js
@@ -1,5 +1,5 @@
 const app = require('./app');
 
-app.listen(process.env.PORT || 80, () => {
-    console.log(`Listening on port ${process.env.PORT || 80}...`);
+app.listen(process.env.PORT || 8080, () => {
+    console.log(`Listening on port ${process.env.PORT || 8080}...`);
 });
