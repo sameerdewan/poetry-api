0b7dad9b82a0143159a8d86e5b04ed9db8d04fc5
sameerdewan <sameer.dewan@itential.com>
2021-03-10 10:23:11 -0500

    user record

---
+++ db/models/User.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+require('mongoose-type-email');
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: mongoose.SchemaTypes.String,
+        unique: true,
+        required: true,
+        min: [3, 'Username is too short'],
+        max: [20, 'Username is too long']
+    },
+    password: {
+        type: mongoose.SchemaTypes.String,
+        required: true,
+        min: [5, 'Password is too short']
+    },
+    email: {
+        type: mongoose.SchemaTypes.Email,
+        unique: true,
+        required: true
+    },
+    validated: {
+        type: mongoose.SchemaTypes.Boolean,
+        default: false
+    },
+    validationCode: {
+        type: mongoose.SchemaTypes.String,
+        required: true
+    }
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
