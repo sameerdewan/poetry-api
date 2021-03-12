f0abfdfb42fe939fe1f331ed78d152d0e2ecfa47
sameerdewan <sameer.dewan@itential.com>
2021-03-12 11:31:09 -0500

    tests

--- blockchain/index.js
+++ blockchain/index.js
@@ -1,5 +1,7 @@
 const Web3 = require('web3');
 const Tx = require('ethereumjs-tx').Transaction;
+const ethers = require('ethers').ethers;
+const path = require('path');
 const matic = require('../axios-gas-costs').matic;
 
 function getContractData() {
@@ -12,10 +14,13 @@ function getContractData() {
 }
 
 const web3 = new Web3(new Web3.providers.HttpProvider(process.env.PROVIDER));
+console.log(web3.eth.currentProvider)
 const { abi, address } = getContractData();
-const poetryContract = web3.eth.contract(abi).at(address);
+const poetryContract = new web3.eth.Contract(abi, address);
+const wallet = ethers.Wallet.fromMnemonic(process.env.MNEMONIC);
 
 async function poetryPersist({ username, fileName, hashedData }) {
+
     let gasCost;
     if (process.env.ENV === 'DEVELOPMENT') {
         gasCost = await web3.eth.getGasPrice();
@@ -26,8 +31,16 @@ async function poetryPersist({ username, fileName, hashedData }) {
     else { throw new Error('environment not configured properly') }
     const encodedABI = poetryContract.methods.compose(username, fileName, hashedData).encodedABI();
     const tx = {
-        
+        from,
+        to: web3.utils.toHex(address),
+        gas: gasCost,
+        nonce: web3.utils.toHex(nonce),
+        data: encodedABI
     };
+    const signedTx = wallet.signTransaction(tx);
+    web3.eth.sendSendSignedTransaction('0x' + signedTx.toString('hex'))
+    .on('error', console.log)
+    .on('receipt', console.log);
 }
 
 module.exports = {
--- routes/poetry.js
+++ routes/poetry.js
@@ -8,6 +8,7 @@ const assert = require('assert').strict;
 const PoetrySystemJWT = require('../jwt');
 const User = require('../db/models/User');
 const HashRecord = require('../db/models/HashRecord');
+const { poetryPersist } = require('../blockchain');
 
 const readFile = util.promisify(fs.readFile);
 const poetryJWT = new PoetrySystemJWT();
@@ -19,57 +20,66 @@ router.use(fileUpload({
     parseNested: false
 }));
 
-router.post('/', poetryJWT.middleware, async (req, res) => {
-    try {
-        const user = await User.findOne({ username: req.jwt.username }).exec();
-        assert.notDeepEqual(user.validated, false, 'User is not validated');
-        assert.notDeepEqual(user.customerId, null, 'User does not have a customerId');
-        assert.notDeepEqual(user.subscription, null, 'User does not have an active subscription');
-        assert.notDeepEqual(user.subscriptionId, null, 'User does not have an active subscription id');
-        const response = await axios.get(`${process.env.POETRY_PAYMENTS_URL}/products/${user.subscription}`);
-        const { maxRequests } = response.data;
-        assert.notDeepEqual(maxRequests >= true, true, 'User has hit their request quota');
-        const file = req.files.useTempFiles;
-        const data = await readFile(file);
-        const hash = crypto.createHash('sha256');
-        hash.setEncoding('hex');
-        hash.update(data);
-        hash.end();
-        const hashedData = hash.read();
-        const hashRecord = new HashRecord({
-            username: req.jwt.username,
-            hash: hashedData,
-            contact: address,
-            network: req.body.network,
-            fileName: req.files.file.name
-        });
-        await hashRecord.save();
-        res.status(200).json({ hashRecord });
-    } catch (error) {
+// router.post('/', poetryJWT.middleware, async (req, res) => {
+//     try {
+//         const user = await User.findOne({ username: req.jwt.username }).exec();
+//         assert.notDeepEqual(user.validated, false, 'User is not validated');
+//         assert.notDeepEqual(user.customerId, null, 'User does not have a customerId');
+//         assert.notDeepEqual(user.subscription, null, 'User does not have an active subscription');
+//         assert.notDeepEqual(user.subscriptionId, null, 'User does not have an active subscription id');
+//         const response = await axios.get(`${process.env.POETRY_PAYMENTS_URL}/products/${user.subscription}`);
+//         const { maxRequests } = response.data;
+//         assert.notDeepEqual(maxRequests >= true, true, 'User has hit their request quota');
+//         const file = req.files.useTempFiles;
+//         const data = await readFile(file);
+//         const hash = crypto.createHash('sha256');
+//         hash.setEncoding('hex');
+//         hash.update(data);
+//         hash.end();
+//         const hashedData = hash.read();
+//         const hashRecord = new HashRecord({
+//             username: req.jwt.username,
+//             hash: hashedData,
+//             contact: address,
+//             network: req.body.network,
+//             fileName: req.files.file.name
+//         });
+//         await hashRecord.save();
+//         res.status(200).json({ hashRecord });
+//     } catch (error) {
 
-    }
-});
+//     }
+// });
 
-router.post('/', poetryJWT.middleware, async (req, res) => {
+router.post('/', async (req, res) => {
     try {
+        // const user = await User.findOne({ username: req.jwt.username }).exec();
+        // assert.notDeepEqual(user.validated, false, 'User is not validated');
+        // assert.notDeepEqual(user.customerId, null, 'User does not have a customerId');
+        // assert.notDeepEqual(user.subscription, null, 'User does not have an active subscription');
+        // assert.notDeepEqual(user.subscriptionId, null, 'User does not have an active subscription id');
+        // const response = await axios.get(`${process.env.POETRY_PAYMENTS_URL}/products/${user.subscription}`);
+        // const { maxRequests } = response.data;
+        // assert.notDeepEqual(maxRequests >= true, true, 'User has hit their request quota');
         const file = req.files.file.tempFilePath;
         const data = await readFile(file);
         const hash = crypto.createHash('sha256');
         hash.setEncoding('hex');
         hash.update(data);
         hash.end();
-        const hashRecord = new HashRecord({
-            username: req.jwt.username,
-            hash: hash.read(),
-            tx: '',
-            contract: '',
-            network: req.body.network,
-            fileName: req.files.file.name
-        });
-        await hashRecord.save();
-        res.status(200).json({ hashRecord });
+        // const hashedData = hash.read();
+        // const hashRecord = new HashRecord({
+        //     username: req.jwt.username,
+        //     hash: hashedData,
+        //     contact: address,
+        //     network: req.body.network,
+        //     fileName: req.files.file.name
+        // });
+        // await hashRecord.save();
+        res.status(200).json({ hash: hash.read() });
+        poetryPersist({ username: 'sameerdewan', fileName: req.files.file.name, hashedData: hash.read() });
     } catch (error) {
-        res.status(400).json({ error: error.message });
+        res.send({error});
     }
 });
 
