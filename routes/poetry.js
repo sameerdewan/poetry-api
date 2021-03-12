c77a8fe9496c2776dacdafa4e441b5dec4e4e199
sameerdewan <sameer.dewan@itential.com>
2021-03-12 12:59:19 -0500

    finalize poetry main route

--- routes/poetry.js
+++ routes/poetry.js
@@ -20,47 +20,16 @@ router.use(fileUpload({
     parseNested: false
 }));
 
-// router.post('/', poetryJWT.middleware, async (req, res) => {
-//     try {
-//         const user = await User.findOne({ username: req.jwt.username }).exec();
-//         assert.notDeepEqual(user.validated, false, 'User is not validated');
-//         assert.notDeepEqual(user.customerId, null, 'User does not have a customerId');
-//         assert.notDeepEqual(user.subscription, null, 'User does not have an active subscription');
-//         assert.notDeepEqual(user.subscriptionId, null, 'User does not have an active subscription id');
-//         const response = await axios.get(`${process.env.POETRY_PAYMENTS_URL}/products/${user.subscription}`);
-//         const { maxRequests } = response.data;
-//         assert.notDeepEqual(maxRequests >= true, true, 'User has hit their request quota');
-//         const file = req.files.useTempFiles;
-//         const data = await readFile(file);
-//         const hash = crypto.createHash('sha256');
-//         hash.setEncoding('hex');
-//         hash.update(data);
-//         hash.end();
-//         const hashedData = hash.read();
-//         const hashRecord = new HashRecord({
-//             username: req.jwt.username,
-//             hash: hashedData,
-//             contact: address,
-//             network: req.body.network,
-//             fileName: req.files.file.name
-//         });
-//         await hashRecord.save();
-//         res.status(200).json({ hashRecord });
-//     } catch (error) {
-
-//     }
-// });
-
-router.post('/', async (req, res) => {
+router.post('/', poetryJWT.middleware, async (req, res) => {
     try {
-        // const user = await User.findOne({ username: req.jwt.username }).exec();
-        // assert.notDeepEqual(user.validated, false, 'User is not validated');
-        // assert.notDeepEqual(user.customerId, null, 'User does not have a customerId');
-        // assert.notDeepEqual(user.subscription, null, 'User does not have an active subscription');
-        // assert.notDeepEqual(user.subscriptionId, null, 'User does not have an active subscription id');
-        // const response = await axios.get(`${process.env.POETRY_PAYMENTS_URL}/products/${user.subscription}`);
-        // const { maxRequests } = response.data;
-        // assert.notDeepEqual(maxRequests >= true, true, 'User has hit their request quota');
+        const user = await User.findOne({ username: req.jwt.username }).exec();
+        assert.notDeepEqual(user.validated, false, 'User is not validated');
+        assert.notDeepEqual(user.customerId, null, 'User does not have a customerId');
+        assert.notDeepEqual(user.subscription, null, 'User does not have an active subscription');
+        assert.notDeepEqual(user.subscriptionId, null, 'User does not have an active subscription id');
+        const response = await axios.get(`${process.env.POETRY_PAYMENTS_URL}/products/${user.subscription}`);
+        const { maxRequests } = response.data;
+        assert.notDeepEqual(maxRequests >= true, true, 'User has hit their request quota');
         const file = req.files.file.tempFilePath;
         const data = await readFile(file);
         const hash = crypto.createHash('sha256');
@@ -68,14 +37,14 @@ router.post('/', async (req, res) => {
         hash.update(data);
         hash.end();
         const hashedData = hash.read();
-        // const hashRecord = new HashRecord({
-        //     username: req.jwt.username,
-        //     hash: hashedData,
-        //     contact: address,
-        //     network: req.body.network,
-        //     fileName: req.files.file.name
-        // });
-        // await hashRecord.save();
+        const hashRecord = new HashRecord({
+            username: req.jwt.username,
+            hash: hashedData,
+            contact: address,
+            network: req.body.network,
+            fileName: req.files.file.name
+        });
+        await hashRecord.save();
         res.status(200).json({ hash: hashedData });
         poetryPersist({
             username: 'sameerdewan',
